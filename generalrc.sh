dcce253721245aae77439098c451607bd012c8b4
Getty Ritter <getty.ritter@gmail.com>
2015-05-09 02:09:58 -0700

    Several updates to both configs

--- generalrc.sh
+++ generalrc.sh
@@ -5,9 +5,9 @@ alias cl=clear
 alias la='ls -a'
 alias ll='ls -l'
 alias lpdup='lpr -o sides=two-sided-long-edge -P hp4200n'
-alias iks='ssh ikshvaku'
-alias ros='ssh rosencrantz'
-alias guil='ssh guildenstern'
+alias iks='command -v mosh 2>&1 1>/dev/null && mosh ikshvaku || ssh ikshvaku'
+alias ros='command -v mosh 2>&1 1>/dev/null && mosh rosencrantz || ssh rosencrantz'
+alias guil='command -v mosh 2>&1 1>/dev/null && mosh guildenstern || ssh guildenstern'
 alias iks-elinks='ssh ikshvaku -t "bash -c \". .bashrc && elinks\""'
 alias iks-finch='ssh ikshvaku -t "bash -c \". .bashrc && finch\""'
 alias iks-irssi='ssh ikshvaku -t "bash -c \". .bashrc && irssi\""'
@@ -18,8 +18,7 @@ alias bigmacs='BIG=true emacs'
 alias textmacs='NARROW=true emacs'
 alias em='EVIL=true BARE=true emacs'
 alias objdump='objdump -M intel'
-alias pr-paper='lpr -P hp4200n -o number-up=2 -o sides=two-sided-long-edge'
-alias bigterm='urxvt -fn "xft:Inconsolata:pixelsize=24"'
+alias bigterm='urxvt -fn "xft:Inconsolata:pixelsize=20"'
 alias hex2raw="tr -d '\\\x' | xxd -r -p"
 
 export EDITOR="emacs -nw"
