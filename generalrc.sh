71f1519cba0a63542e0d58a3169c1559ce542afd
Getty Ritter <gettyritter@gmail.com>
2018-03-28 18:01:51 -0700

    Some exa aliases, a bigger history

--- generalrc.sh
+++ generalrc.sh
@@ -2,8 +2,8 @@
 
 # User specific aliases and functions
 alias cl=clear
-alias la='ls -a'
-alias ll='ls -l'
+alias la='exa -a'
+alias ll='exa --git -l'
 alias lpdup='lpr -o sides=two-sided-long-edge -P hp4200n'
 alias ros='command -v mosh 2>&1 1>/dev/null && mosh rosencrantz || ssh rosencrantz'
 alias guil='command -v mosh 2>&1 1>/dev/null && mosh guildenstern || ssh guildenstern'
@@ -13,8 +13,9 @@ alias bigmacs='BIG=true emacs'
 alias textmacs='NARROW=true emacs'
 alias em='EVIL=true BARE=true emacs'
 alias objdump='objdump -M intel'
-alias bigterm='urxvt -fn "xft:Inconsolata:pixelsize=20"'
+alias bigterm='urxvt -fn "xft:Fira Code:size=28"'
 alias hex2raw="tr -d '\\\x' | xxd -r -p"
 
 export EDITOR="emacs -nw"
 export BASHFILESIZE=50000
+export HISTSIZE=5000
